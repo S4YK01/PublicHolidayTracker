4a03d1e64485fcf492f055271ffed6e361b4c517
S4YK01 <ceyhunsener3452@gmail.com>
2025-12-05 18:32:13 +0300

    Delete PublicHolidayTracker directory

--- PublicHolidayTracker/Holiday.cs
+++
@@ -1,9 +0,0 @@
-public class Holiday
-{
-    public string date { get; set; }
-    public string localName { get; set; }
-    public string name { get; set; }
-    public string countryCode { get; set; }
-    public bool fixed { get; set; }
-    public bool global { get; set; }
-}
